d02eb010a2a25450f16fa6092895ec1b64b7014d
Zach Parent <zachparent@duck.com>
2026-04-26 20:40:46 -0700

    Use static Astro build for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
-export default defineConfig({
-  adapter: cloudflare({
-    prerenderEnvironment: 'node',
-
-    platformProxy: {
-      enabled: true
-    },
-
-    imageService: "cloudflare"
-  })
-});
+export default defineConfig({});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,1 @@
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
-
-declare namespace App {
-	interface Locals extends Runtime {}
-}
+/// <reference types="astro/client" />
